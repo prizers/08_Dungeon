e7bd4676d13d110cc9c4fa5003b0673d2ed061a3
Lev V. Babchenko <gour@mail.ru>
2018-09-04 19:03:27 +0500

    initial commit

---
+++ BfsTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon
+{
+	public class BfsTask
+	{
+		public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Point[] chests)
+		{
+			yield break;
+		}
+	}
+}
